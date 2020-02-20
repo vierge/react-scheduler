f80a4afe7b02ad4be5248d28c7907ffdda684026
vierge <marancholia@gmail.com>
2020-02-20 14:08:53 -0500

    Api start yay

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+
 import DayList from "./DayList"
 import Appointment from "components/Appointment/index.jsx"
 
+
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = [
   {
@@ -63,6 +51,17 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days') 
+    .then(res => {
+      console.log({ days: res.data} );
+      setDays(res.data);
+    })
+    .catch(err => console.log(err, err.stack));
+  }, []);
+
   // const { appointments } = props;
   const mappedSchedule = appointments.map(thisAppt => {
     return(
@@ -75,7 +74,7 @@ export default function Application(props) {
       <section className="sidebar">
         <img
           className="sidebar--centered"
-          src="images/logo.png"
+          src="/images/logo.png"
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
